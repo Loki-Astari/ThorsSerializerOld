352f3c7a0842b40cc5de63483004f785a6f4257a
Loki Astari <Loki.Astari@gmail.com>
2024-09-11 23:23:04 -0700

    Add PreFlight processes for strings.
        When printing to strings add pre-flight.
        Do an initial print to calculate the size.
        Use this information to reserve the correct space in the string.
        Then do the actual printing.

--- src/Serialize/StringOutput.h
+++ src/Serialize/StringOutput.h
@@ -3,6 +3,7 @@
 
 #include "SerializeConfig.h"
 #include <string>
+#include <charconv>
 
 namespace ThorsAnvil::Serialize
 {
@@ -10,15 +11,24 @@ namespace ThorsAnvil::Serialize
 struct StringOutput
 {
     std::string&    data;
+    std::size_t     calcSize;
     bool            ok;
+    bool            preFlight;
 
     public:
         StringOutput(std::string& output)
             : data(output)
+            , calcSize(0)
             , ok(true)
+            , preFlight(false)
         {}
         bool write(char const* src, std::size_t size)
         {
+            if (preFlight)
+            {
+                calcSize += size;
+                return true;
+            }
             data += std::string_view(src, size);
             return true;
         }
@@ -33,8 +43,27 @@ struct StringOutput
         template<typename T>
         void writeValue(T const& src)
         {
-            using std::to_string;
-            data += to_string(src);
+            using std::to_chars;
+            static char buffer[100];
+            std::to_chars_result    result = to_chars(buffer, buffer + 100, src);
+            if (preFlight)
+            {
+                calcSize += (result.ptr - buffer);
+            }
+            else
+            {
+                data += std::string_view(buffer, (result.ptr - buffer));
+            }
+        }
+        bool preflightSize()
+        {
+            preFlight = true;
+            return true;
+        }
+        void reserveSize()
+        {
+            data.reserve(calcSize + 10);
+            preFlight = false;
         }
 };
 
